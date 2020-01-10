195ee6afeafb8cbf0820a71596fc687187922347
Friedrich Lindenberg <friedrich@pudo.org>
2020-01-10 08:57:45 +0100

    Bump version: 0.1.5 → 0.1.6

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as f:
 
 setup(
     name='dbcopy',
-    version='0.1.5',
+    version='0.1.6',
     description="Copy contents of a SQL database to another",
     long_description=long_description,
     long_description_content_type='text/markdown',
